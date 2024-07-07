e2158aa953102a31aa73581bda863753d8a89c27
cbpilthem <150981304+cbpilthem@users.noreply.github.com>
2024-07-06 20:11:56 -0500

    Update google-icons.js

--- google-icons.js
+++ google-icons.js
@@ -1,6 +1,6 @@
 (function() {
-var style=document.querySelectorAll('head style, head link'), append=(a)=>{a=>document.head.appendChild(a)};
-if(style[0]) append=(a)=>{a=>style[0].insertAdjacentHTML('afterend', a)};
+var style=document.querySelectorAll('head style, head link'), append=(a)=>{document.head.appendChild(a)};
+if(style[0]) append=(a)=>{style[0].insertAdjacentHTML('afterend', a)};
 const doc = new DOMParser().parseFromString(`<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=${window.GOOGLE_FONTS||'Roboto'}:wght@300;400;500;700&display=swap" />
