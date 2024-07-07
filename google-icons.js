f12a1223ec0a4ed647a1d75cd45adab49ac00422
cbpilthem <150981304+cbpilthem@users.noreply.github.com>
2024-07-06 20:05:19 -0500

    Update google-icons.js

--- google-icons.js
+++ google-icons.js
@@ -1,4 +1,7 @@
 (function() {
+var style=doc.querySelectorAll('head style, head link'), append=(a)=>{a=>document.head.appendChild(a)};
+if(style[0]) append=(a)=>{a=>style[0].insertAdjacentHTML('afterend', a)};
+element.insertAdjacentHTML('beforeend', '<p>New paragraph</p>');
 const doc = new DOMParser().parseFromString(`<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=${window.GOOGLE_FONTS||'Roboto'}:wght@300;400;500;700&display=swap" />
