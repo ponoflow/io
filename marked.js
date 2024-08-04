0140e119340b15a4b38a79f185f4830f401e6cef
cbpilthem <150981304+cbpilthem@users.noreply.github.com>
2024-08-04 02:05:30 -0500

    Update marked.js

--- marked.js
+++ marked.js
@@ -81,6 +81,7 @@ class MarkedClass {
 var marked = new MarkedClass();
 var ezMarkup = (el, text)=>{
 	text=decodeHexEntities(indentCodeMD(text||el.innerHTML));
+	text=text.split('\"').join('"');
 	el.innerHTML=marked.parse(text);
 	Prism.highlightAll();
 };
